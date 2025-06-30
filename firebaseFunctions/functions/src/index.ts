873235be47a65cdb443e020463dfc60ea91a291b
Iago <ifavilla@gmail.com>
2025-06-30 17:34:20 -0300

    Preparando para separar dados de programação

--- firebaseFunctions/functions/src/index.ts
+++ firebaseFunctions/functions/src/index.ts
@@ -4,8 +4,6 @@ import { initializeApp } from "firebase-admin/app";
 import { getStorage } from "firebase-admin/storage";
 import * as fs from "fs";
 import * as path from "path";
-import { readExcelFile } from "./readExcelFile"; // Implemente esta função para ler o Excel
-import { processRegion } from "./processRegion"; // Implemente esta função para processar as regiões
 import * as logger from "firebase-functions/logger";
 import * as os from "os";
 import Busboy from "busboy";
