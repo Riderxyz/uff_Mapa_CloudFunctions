7fa07743f9520864054f353d78001feee8e12e29
Iago <ifavilla@gmail.com>
2025-07-14 11:42:53 -0300

    Teste de deploy de funções para PROD

--- firebaseFunctions/functions/src/index.ts
+++ firebaseFunctions/functions/src/index.ts
@@ -1,37 +1,51 @@
 /* import { onRequest } from "firebase-functions/v2/https"; */
 import { initializeApp } from "firebase-admin/app";
 import { onSchedule } from "firebase-functions/scheduler";
-import { atualizandoProgramacao } from "./atualizadores/programacao";
+/* import { atualizandoProgramacao } from "./atualizadores/programacao";
 import { atualizandoStatus } from "./atualizadores/status";
 import { atualizandoVisitas } from "./atualizadores/visitas";
-import { CloudFunctionResponse } from "./interface/cloudFunctionResponse.interface";
+import { CloudFunctionResponse } from "./interface/cloudFunctionResponse.interface"; */
 
 // Inicializa o Firebase Admin SDK
 initializeApp();
 
-export const atualizarDados = onSchedule("0 */1 * * 1-5", async (event) => {
-  const result = await atualizandoProgramacao();
-  console.log("Resultado:", result);
-});
 
-export const atualizarDadoFull = onSchedule("0 */1 * * 1-5", async (event) => {
-  const resultProgramacao: CloudFunctionResponse = await atualizandoProgramacao();
-  const resultStatus: CloudFunctionResponse = await atualizandoStatus();
-  const resultVisitas: CloudFunctionResponse = await atualizandoVisitas();
+export const atualizarDadoFull = onSchedule("0 6-22 * * 1-5", async (event) => {
+const funcoesEmOrdem: (() => Promise<CloudFunctionResponse>)[] = [
+    atualizandoProgramacao,
+    atualizandoVisitas,
+    atualizandoStatus,
+    atualizandoDashboardData,
+  ];
 
-  Promise.allSettled([resultProgramacao, resultStatus, resultVisitas]).then(
-    (results) => {
-      results.forEach((result, index) => {
-        if (result.status === "fulfilled") {
+  from(funcoesEmOrdem)
+    .pipe(
+      concatMap((fn) => from(fn())),
+      toArray(),
+      tap((resultados: CloudFunctionResponse[]) => {
+        const total = resultados.length;
+        const sucesso = resultados.filter((r) => r.success).length;
+        const falhas = total - sucesso;
+
+        console.log(`✅ Sucesso: ${sucesso}, ❌ Falhas: ${falhas}`);
+
+        if (sucesso === total) {
+          console.log("🟢 Todas as funções executadas com sucesso!");
+        } else if (falhas === total) {
+          console.log("🔴 Todas falharam!");
+        } else {
+          console.log("🟡 Algumas funções falharam...");
         }
-      });
-    }
-  );
-  console.log("Resultados:", {
-    resultProgramacao,
-    resultStatus,
-    resultVisitas,
-  });
+
+        // Dispara log final
+      }),
+      switchMap((resultados) => atualizarLog(resultados))
+    )
+    .subscribe({
+      complete: () => console.log("🏁 Execução sequencial completa!"),
+      error: (err) => console.error("🚨 Erro inesperado:", err),
+    });
+}
 });
 
 
